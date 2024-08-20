07ec89ea3877d020c2ea848cfa4248dc77b76c2a
isaias araujo <araujoisaias58@gmail.com>
2024-08-20 15:29:23 -0300

    Update App.java

--- WHILE/Ex2/src/App.java
+++ WHILE/Ex2/src/App.java
@@ -5,15 +5,14 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int codigo;
-       int a = 0;
-       int b = 0;
-       int c = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
         Scanner sc = new Scanner(System.in);
 
         do {
             System.out.println("bem vindo ao posto wano");
-            System.out.println(
-                    "para participar da nossa pesquisa basta apenas digitar o codigo de acordo com o seu tipo de combustivel");
+            System.out.println("para participar da nossa pesquisa basta apenas digitar o codigo de acordo com o seu tipo de combustivel");
             System.out.println("1 = alcool");
             System.out.println("2 = gasolina");
             System.out.println("3 = disel");
