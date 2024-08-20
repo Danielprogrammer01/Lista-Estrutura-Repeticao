87ea59f973aed537b733c197adc4be8a355fa8f0
Daniel Correia <130627373+Danielprogrammer01@users.noreply.github.com>
2024-08-20 15:15:13 -0300

    Add files via upload

---
+++ WHILE/Ex2/src/App.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        public final static void clearConsole()
+        {
+            try
+            {
+                final String os = System.getProperty("os.name");
+        
+                if (os.contains("Windows"))
+                {
+                    Runtime.getRuntime().exec("cls");
+                }
+                else
+                {
+                    Runtime.getRuntime().exec("clear");
+                }
+            }
+            catch (final Exception e)
+            {
+                //  Handle any exceptions.
+            }
+        }
+
+        int codigo;
+       int a = 0;
+       int b = 0;
+       int c = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("bem vindo ao posto wano");
+            System.out.println(
+                    "para participar da nossa pesquisa basta apenas digitar o codigo de acordo com o seu tipo de combustivel");
+            System.out.println("1 = alcool");
+            System.out.println("2 = gasolina");
+            System.out.println("3 = disel");
+            System.out.println("digite 4 para encerrar:");
+            codigo = sc.nextInt();
+            clearConsole();
+            if (codigo == 1) {
+                a++;
+            }
+    
+            if (codigo == 2) {
+                b++;
+            }
+    
+            if (codigo == 3) {
+                c++;
+            }
+        } while (codigo != 4);
+
+
+       System.out.println("muito obrigado");
+       System.out.println("quantidade votada");
+       System.out.printf("alcool: %d %n", a);
+       System.out.printf("gasolina: %d %n", b);
+       System.out.printf("disel: %d", c);
+
+        sc.close();
+    }
+}
